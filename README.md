ce32059ca7d709b1c31bc805c97f93bfe19a8c11
Gabriel Souza <gabrieldesouza2305@gmail.com>
2022-08-19 14:39:32 -0300

    up

---
+++ README.md
@@ -0,0 +1,19 @@
+# OpenGL Game Engine Development in Visual Studio Code (requirements)
+Para garantir a boa execução do openGL no sistema operacional Linux, certifique-se de que estes pacotes estão baixados...
+
+
+```
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install freeglut3
+    sudo apt-get install freeglut3-dev
+    sudo apt-get install binutils-gold
+    sudo apt-get install g++
+    sudo apt-get install g++ cmake
+    sudo apt-get install libglew-dev
+    sudo apt-get install mesa-common-dev
+    sudo apt-get install mesa-common-dev
+    sudo apt-get install libglew1.5-dev libglm-dev
+    sudo apt-get install mesa-utilsx
+    sudo apt-get install libsdl2{,-mixer,-image,-ttf}-dev
+```
+
---
+++ common.h
@@ -0,0 +1,30 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+/*=======STANDARD TEMPLATE LIBRARY=======*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/*=======GLU/GLEW=======*/
+#include "./Dependencies/include/GL/glew.h"
+#include "./Dependencies/include/GL/glu.h"
+#include "./Dependencies/include/GL/gl.h"
+
+/*=======SDL2=======*/
+#include "./Dependencies/include/SDL2/SDL.h"
+#include "./Dependencies/include/SDL2/SDL_opengl.h"
+#include "./Dependencies/include/SDL2/SDL_ttf.h"
+#include "./Dependencies/include/SDL2/SDL_image.h"
+
+/*=======CLASS DEFINITIONS=======*/
+#include "./Dependencies/statics/glfw/include/GLFW/glfw3.h"
+#include "./Dependencies/statics/glfw/include/GLFW/glfw3native.h"
+
+#include "./Dependencies/statics/glm/glm/glm.hpp"
+#include "./Dependencies/statics/glm/glm/gtx/string_cast.hpp"
+#include "./Dependencies/statics/stb/stb_image.h"
+
+#include "./Dependencies/statics/glew/include/GL/glew.h"
+
+
+#endif
